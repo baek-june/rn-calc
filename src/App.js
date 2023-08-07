7d4755b9e884ca1301082c35b95b3012abcb0c8e
성백준 <toypiano@naver.com>
2023-08-07 10:24:36 +0900

    work in macbook2

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Button from "./components/Button";
+
+const App = () => {
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+            <Text style={styles.text}>Calc App</Text>
+            <Button onPress={() => console.log("click!")} color="purple" />
+            <Button title="title" />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    text: {
+        fontSize: 30,
+        fontWeight: "700",
+        color: "green",
+    },
+    error: {
+        color: "red",
+    },
+});
+
+export default App;
